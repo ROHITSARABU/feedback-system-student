03b5cdfae580f46df501136d6a3c871fc57433cf
Arulsarabu <arulsarabu1719@gmail.com>
2018-08-03 22:23:39 +0530

    My project setup completed successfully

---
+++ feedback_backend/src/main/java/com/my/feedback_backend/App.java
@@ -0,0 +1,13 @@
+package com.my.feedback_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
